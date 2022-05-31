9475de70896bdfdda755633fb7b4515b8476dc3e
Sajid Javed <engr.maliksajidkhan@gmail.com>
2022-05-31 23:29:09 +0400

    initial commit

---
+++ FunPlugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Fun Plugin
+* Plugin URI: https://github.com/author/repo.git
+* Description: This is a Fun Plugin.
+* Version: 1.0
+* Author: Sajid Javed
+* Author URI: https://sajidjaved.com/
+**/
