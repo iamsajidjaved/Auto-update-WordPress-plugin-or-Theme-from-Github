7353e457cfc4d7d20b828fc3c51f207620890439
Sajid Javed <engr.maliksajidkhan@gmail.com>
2022-05-31 23:54:05 +0400

    2.0 version

--- FunPlugin.php
+++ FunPlugin.php
@@ -1,9 +1,22 @@
 <?php
 /**
 * Plugin Name: Fun Plugin
-* Plugin URI: https://github.com/author/repo.git
+* Plugin URI: https://github.com/iamsajidjaved/FunPlugin
 * Description: This is a Fun Plugin.
-* Version: 1.0
+* Version: 2.0
 * Author: Sajid Javed
 * Author URI: https://sajidjaved.com/
 **/
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/iamsajidjaved/FunPlugin',
+	__FILE__,
+	'FunPlugin'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('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');
